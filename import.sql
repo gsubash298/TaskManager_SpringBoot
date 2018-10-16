73fe05107bfa3af9290deb61c617c8753d4ee641
gsubash298 <gsubash298@gmail.com>
2018-10-16 11:01:05 +0530

    Add files via upload

---
+++ import.sql
@@ -0,0 +1,3 @@
+<!-- CREATE TABLE parenttasktable (Parent_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, Parent_Task VARCHAR(250)) -->
+
+<!-- CREATE TABLE tasktable (Task_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, Parent_ID INT, Task VARCHAR(250), Start_Date DATE, End_Date DATE, Priority INT) -->
